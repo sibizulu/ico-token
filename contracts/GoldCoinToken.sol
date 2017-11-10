0b9bd7387ddc1e17091916ee454ae464150c1bc8
sibizulu <sibiraj@lightrains.com>
2017-11-10 10:32:11 +0530

    Adding contract for token

---
+++ contracts/GoldCoinToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+
+contract GoldCoinToken is MintableToken{
+  string public constant name = "Gold Coin";
+  string public constant symbol = "GDC";
+  uint8 public constant decimals = 18;
+}
